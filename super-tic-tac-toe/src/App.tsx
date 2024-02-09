fd69e41321b4a5d07f0ea8ec2c26d0afdcfb4796
Izuchukwu <barth.poetic@gmail.com>
2024-02-09 10:42:31 -0600

    xyz

--- super-tic-tac-toe/src/App.tsx
+++ super-tic-tac-toe/src/App.tsx
@@ -1,8 +1,47 @@
 import React from "react";
 import "./App.css";
+import { boardArray } from "./board";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App: React.FC = () => {
+  const handleClick: React.MouseEventHandler<HTMLDivElement> = (event) => {
+    const clickedBox = event.currentTarget as HTMLDivElement;
+
+    let boardText = (clickedBox.textContent = "");
+    // const index: number = parseInt(clickedBox.dataset.id || "", 10);
+    // let boardText = boardArray[index - 1].text;
+    // boardText = clickedBox.textContent;
+
+    let xArray: any = [];
+    let oArray: any = [];
+
+    if (oArray.length <= xArray.length) {
+      clickedBox.textContent = "O";
+    } else clickedBox.textContent = "X";
+
+    for (let i = 0; i < boardArray.length; i++) {
+      if (boardText === "X") {
+        xArray.push(boardText);
+      } else if (boardText === "Y") {
+        oArray.push(boardText);
+      }
+    }
+  };
+  return (
+    <div className="App">
+      <div className="main-box">
+        {boardArray.map((item) => (
+          <div
+            key={item.id}
+            className={item.className}
+            onClick={handleClick}
+            data-id={item.id}
+          >
+            {/* {item.textContent} */}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default App;
---
+++ super-tic-tac-toe/src/board.ts
@@ -0,0 +1,21 @@
+export const boardArray: [
+  { id: number; className: string; filled: boolean; text: string },
+  { id: number; className: string; filled: boolean; text: string },
+  { id: number; className: string; filled: boolean; text: string },
+  { id: number; className: string; filled: boolean; text: string },
+  { id: number; className: string; filled: boolean; text: string },
+  { id: number; className: string; filled: boolean; text: string },
+  { id: number; className: string; filled: boolean; text: string },
+  { id: number; className: string; filled: boolean; text: string },
+  { id: number; className: string; filled: boolean; text: string }
+] = [
+  { id: 1, className: "second-box top-left", filled: false, text: "" },
+  { id: 2, className: "second-box top-mid", filled: false, text: "" },
+  { id: 3, className: "second-box top-right", filled: false, text: "" },
+  { id: 4, className: "second-box mid-left", filled: false, text: "" },
+  { id: 5, className: "second-box mid-mid", filled: false, text: "" },
+  { id: 6, className: "second-box mid-right", filled: false, text: "" },
+  { id: 7, className: "second-box right-left", filled: false, text: "" },
+  { id: 8, className: "second-box right-mid", filled: false, text: "" },
+  { id: 9, className: "second-box right-right", filled: false, text: "" },
+];
