5d3f7d54cdf07197850fa089dfcebf5a6ee73077
Izuchukwu <barth.poetic@gmail.com>
2024-02-12 14:24:54 -0600

    build

--- super-tic-tac-toe/src/App.tsx
+++ super-tic-tac-toe/src/App.tsx
@@ -6,25 +6,36 @@ const App: React.FC = () => {
   const handleClick: React.MouseEventHandler<HTMLDivElement> = (event) => {
     const clickedBox = event.currentTarget as HTMLDivElement;
 
-    let boardText = (clickedBox.textContent = "");
+    // let boxText = clickedBox.textContent;
     // const index: number = parseInt(clickedBox.dataset.id || "", 10);
     // let boardText = boardArray[index - 1].text;
     // boardText = clickedBox.textContent;
 
-    let xArray: any = [];
-    let oArray: any = [];
-
-    if (oArray.length <= xArray.length) {
-      clickedBox.textContent = "O";
-    } else clickedBox.textContent = "X";
-
-    for (let i = 0; i < boardArray.length; i++) {
-      if (boardText === "X") {
-        xArray.push(boardText);
-      } else if (boardText === "Y") {
-        oArray.push(boardText);
-      }
+    if (clickedBox.textContent === "X" || clickedBox.textContent === "O") {
+      return;
     }
+
+    let mainBox = document.querySelectorAll(".main-box");
+    let xCount: any = Array.from(mainBox).filter(
+      (box) => box.textContent === "X"
+    ).length;
+    let oCount: any = Array.from(mainBox).filter(
+      (box) => box.textContent === "O"
+    ).length;
+
+    if ((xCount === 0 && oCount === 0) || xCount === oCount) {
+      clickedBox.textContent = "X";
+    } else if (xCount > oCount) clickedBox.textContent = "O";
+    // if (oArray.length <= xArray.length) {
+    //   clickedBox.textContent = "O";
+    // } else clickedBox.textContent = "X";
+
+    // for (let i = 0; i < boardArray.length; i++) {
+    //   if (boardText === "X") {
+    //     xArray.push(boardText);
+    //   } else if (boardText === "O") {
+    //     oArray.push(boardText);
+    //   }
   };
   return (
     <div className="App">
