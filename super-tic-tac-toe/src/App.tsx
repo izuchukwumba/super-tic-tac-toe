77e71f41792d90441f7bea58b8fa68839e48ce1c
Izuchukwu Mba <izuchukwu.mba@talladega.edu>
2024-02-15 17:45:53 -0600

    more commit

--- super-tic-tac-toe/src/App.tsx
+++ super-tic-tac-toe/src/App.tsx
@@ -1,31 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { boardArray } from "./board";
 
 const App: React.FC = () => {
+  const [xArray, setXArray] = useState<HTMLDivElement[]>([]);
+  const [oArray, setOArray] = useState<HTMLDivElement[]>([]);
+  const [updatedBoardArray, setUpdatedBoardArray] = useState(boardArray);
+  const [winner, setWinner] = useState<string | null>(null);
+
   const handleClick: React.MouseEventHandler<HTMLDivElement> = (event) => {
     const clickedBox = event.currentTarget as HTMLDivElement;
 
+    // updateArrays(clickedBox);
+    // checkWinner();
+
+    if (clickedBox.textContent === "X" || clickedBox.textContent === "O") {
+      return;
+    }
+    if (winner) {
+      return;
+    }
     // let boxText = clickedBox.textContent;
     // const index: number = parseInt(clickedBox.dataset.id || "", 10);
     // let boardText = boardArray[index - 1].text;
     // boardText = clickedBox.textContent;
 
-    if (clickedBox.textContent === "X" || clickedBox.textContent === "O") {
-      return;
-    }
+    const allBoxes = document.querySelectorAll(".box");
+    // let xArray = Array.from(allBoxes).filter((box) => box.textContent === "X");
+    // let oArray = Array.from(allBoxes).filter((box) => box.textContent === "O");
+    let xCount: any = xArray.length;
+    let oCount: any = oArray.length;
 
-    let mainBox = document.querySelectorAll(".main-box");
-    let xCount: any = Array.from(mainBox).filter(
-      (box) => box.textContent === "X"
-    ).length;
-    let oCount: any = Array.from(mainBox).filter(
-      (box) => box.textContent === "O"
-    ).length;
-
-    if ((xCount === 0 && oCount === 0) || xCount === oCount) {
+    if (xCount === 0 || xCount === oCount) {
       clickedBox.textContent = "X";
-    } else if (xCount > oCount) clickedBox.textContent = "O";
+      setXArray((prev) => [...prev, clickedBox]);
+      // setUpdatedBoardArray({ ...prev, text: clickedBox.textContent });
+    } else {
+      clickedBox.textContent = "O";
+      setOArray((prev) => [...prev, clickedBox]);
+    }
     // if (oArray.length <= xArray.length) {
     //   clickedBox.textContent = "O";
     // } else clickedBox.textContent = "X";
@@ -36,18 +49,125 @@ const App: React.FC = () => {
     //   } else if (boardText === "O") {
     //     oArray.push(boardText);
     //   }
+
+    // console.log(xCount, oCount);
+    // console.log(oCount);
+    // console.log(["yam"]);
+
+    checkWinner();
+    console.log(updatedBoardArray);
+
+    // setWinner(null);
   };
+
+  function checkWinner() {
+    if (
+      (xArray.some((box) => box.classList.contains("top-left")) &&
+        xArray.some((box) => box.classList.contains("top-mid")) &&
+        xArray.some((box) => box.classList.contains("top-right"))) ||
+      (xArray.some((box) => box.classList.contains("mid-left")) &&
+        xArray.some((box) => box.classList.contains("mid-mid")) &&
+        xArray.some((box) => box.classList.contains("mid-right"))) ||
+      (xArray.some((box) => box.classList.contains("bottom-left")) &&
+        xArray.some((box) => box.classList.contains("bottom-mid")) &&
+        xArray.some((box) => box.classList.contains("bottom-right"))) ||
+      (xArray.some((box) => box.classList.contains("top-left")) &&
+        xArray.some((box) => box.classList.contains("mid-left")) &&
+        xArray.some((box) => box.classList.contains("bottom-left"))) ||
+      (xArray.some((box) => box.classList.contains("top-mid")) &&
+        xArray.some((box) => box.classList.contains("mid-mid")) &&
+        xArray.some((box) => box.classList.contains("bottom-mid"))) ||
+      (xArray.some((box) => box.classList.contains("top-right")) &&
+        xArray.some((box) => box.classList.contains("mid-right")) &&
+        xArray.some((box) => box.classList.contains("bottom-right"))) ||
+      (xArray.some((box) => box.classList.contains("top-left")) &&
+        xArray.some((box) => box.classList.contains("mid-mid")) &&
+        xArray.some((box) => box.classList.contains("bottom-right"))) ||
+      (xArray.some((box) => box.classList.contains("top-right")) &&
+        xArray.some((box) => box.classList.contains("mid-mid")) &&
+        xArray.some((box) => box.classList.contains("bottom-left")))
+    ) {
+      console.log("X wins");
+      endGame();
+      setWinner("X");
+    } else if (
+      (oArray.some((box) => box.classList.contains("top-left")) &&
+        oArray.some((box) => box.classList.contains("top-mid")) &&
+        oArray.some((box) => box.classList.contains("top-right"))) ||
+      (oArray.some((box) => box.classList.contains("mid-left")) &&
+        oArray.some((box) => box.classList.contains("mid-mid")) &&
+        oArray.some((box) => box.classList.contains("mid-right"))) ||
+      (oArray.some((box) => box.classList.contains("bottom-left")) &&
+        oArray.some((box) => box.classList.contains("bottom-mid")) &&
+        oArray.some((box) => box.classList.contains("bottom-right"))) ||
+      (oArray.some((box) => box.classList.contains("top-left")) &&
+        oArray.some((box) => box.classList.contains("mid-left")) &&
+        oArray.some((box) => box.classList.contains("bottom-left"))) ||
+      (oArray.some((box) => box.classList.contains("top-mid")) &&
+        oArray.some((box) => box.classList.contains("mid-mid")) &&
+        oArray.some((box) => box.classList.contains("bottom-mid"))) ||
+      (oArray.some((box) => box.classList.contains("top-right")) &&
+        oArray.some((box) => box.classList.contains("mid-right")) &&
+        oArray.some((box) => box.classList.contains("bottom-right"))) ||
+      (oArray.some((box) => box.classList.contains("top-left")) &&
+        oArray.some((box) => box.classList.contains("mid-mid")) &&
+        oArray.some((box) => box.classList.contains("bottom-right"))) ||
+      (oArray.some((box) => box.classList.contains("top-right")) &&
+        oArray.some((box) => box.classList.contains("mid-mid")) &&
+        oArray.some((box) => box.classList.contains("bottom-left")))
+    ) {
+      console.log("O Wins");
+      endGame();
+      setWinner("O");
+    }
+  }
+  function endGame() {
+    const endGameArray = updatedBoardArray.map((item) => {
+      return { ...item, text: "" };
+
+      // updatedBoardArray.map((item) => (item.textContent = ""));
+    });
+
+    setUpdatedBoardArray(endGameArray);
+    // setXArray([]);
+    // setOArray([]);
+    console.log("Game ended");
+    console.log(updatedBoardArray);
+    return true;
+  }
+
+  useEffect(() => {
+    // This will run after the component has re-rendered due to state changes
+    // Any action that needs to be performed after the state has been updated can be placed here
+    console.log(xArray.length, oArray.length);
+
+    setUpdatedBoardArray((prevBoardArray) => {
+      return prevBoardArray.map((item) => {
+        if (xArray.some((box) => box.classList.contains(item.className))) {
+          return { ...item, text: "X" };
+        } else if (
+          oArray.some((box) => box.classList.contains(item.className))
+        ) {
+          return { ...item, text: "O" };
+        } else {
+          return { ...item, text: "" };
+        }
+      });
+    });
+    checkWinner();
+  }, [xArray, oArray]);
+
   return (
     <div className="App">
       <div className="main-box">
-        {boardArray.map((item) => (
+        {updatedBoardArray.map((item) => (
           <div
             key={item.id}
             className={item.className}
             onClick={handleClick}
             data-id={item.id}
           >
-            {/* {item.textContent} */}
+            {!winner || item.text}
           </div>
         ))}
       </div>
--- super-tic-tac-toe/src/board.ts
+++ super-tic-tac-toe/src/board.ts
@@ -1,21 +1,16 @@
-export const boardArray: [
-  { id: number; className: string; filled: boolean; text: string },
-  { id: number; className: string; filled: boolean; text: string },
-  { id: number; className: string; filled: boolean; text: string },
-  { id: number; className: string; filled: boolean; text: string },
-  { id: number; className: string; filled: boolean; text: string },
-  { id: number; className: string; filled: boolean; text: string },
-  { id: number; className: string; filled: boolean; text: string },
-  { id: number; className: string; filled: boolean; text: string },
-  { id: number; className: string; filled: boolean; text: string }
-] = [
-  { id: 1, className: "second-box top-left", filled: false, text: "" },
-  { id: 2, className: "second-box top-mid", filled: false, text: "" },
-  { id: 3, className: "second-box top-right", filled: false, text: "" },
-  { id: 4, className: "second-box mid-left", filled: false, text: "" },
-  { id: 5, className: "second-box mid-mid", filled: false, text: "" },
-  { id: 6, className: "second-box mid-right", filled: false, text: "" },
-  { id: 7, className: "second-box right-left", filled: false, text: "" },
-  { id: 8, className: "second-box right-mid", filled: false, text: "" },
-  { id: 9, className: "second-box right-right", filled: false, text: "" },
+export const boardArray: Array<{
+  id: number;
+  className: string;
+  filled: boolean;
+  text: string;
+}> = [
+  { id: 1, className: "box top-left", filled: false, text: "" },
+  { id: 2, className: "box top-mid", filled: false, text: "" },
+  { id: 3, className: "box top-right", filled: false, text: "" },
+  { id: 4, className: "box mid-left", filled: false, text: "" },
+  { id: 5, className: "box mid-mid", filled: false, text: "" },
+  { id: 6, className: "box mid-right", filled: false, text: "" },
+  { id: 7, className: "box bottom-left", filled: false, text: "" },
+  { id: 8, className: "box bottom-mid", filled: false, text: "" },
+  { id: 9, className: "box bottom-right", filled: false, text: "" },
 ];
